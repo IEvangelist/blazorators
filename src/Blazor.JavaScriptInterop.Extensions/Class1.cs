0eb1ee1ddc7d9acfc80c7cbb81c5ecbe97a7f7ff
David Pine <david.pine@microsoft.com>
2021-10-12 10:04:24 -0500

    Initial bits

---
+++ src/Blazor.JavaScriptInterop.Extensions/Class1.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) David Pine. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+
+namespace Microsoft.JSInterop.Extensions;
+
+public static partial class IJSRuntimeExtensions
+{
+    //public static partial ValueTask GetCurrentPositionAsync(this IJSRuntime jSRuntime)
+    //{
+    //    return ValueTask.CompletedTask;
+    //}
+}
---
+++ src/Blazor.SourceGenerators/JavaScriptInteropSourceGenerator.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) David Pine. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Linq;
+
+namespace blazorators
+{
+    [Generator]
+    public class JavaScriptInteropSourceGenerator : ISourceGenerator
+    {
+        void ISourceGenerator.Execute(GeneratorExecutionContext context)
+        {
+            // STEPS:
+            // 1. Warn if not referencing Microsoft.JSInterop.
+            // 2. Determine if we should be adding generated source.
+            // 3. Add generated source.
+
+            // 1.
+            if (!context.Compilation.ReferencedAssemblyNames.Any(
+                ai => ai.Name.Equals("Microsoft.JSInterop", StringComparison.OrdinalIgnoreCase)))
+            {
+                // TODO: report warning that the consuming lib needs to reference this library
+
+                //context.ReportDiagnostic(
+                //    Diagnostic.Create(
+                //        new DiagnosticDescriptor(
+                //            1,
+                //            ))
+            }
+
+            // 2.
+            // TODO:
+
+            // 3.
+            context.AddSource(
+                "javascript.interop.cs",
+                SourceText.From(""));
+
+            throw new NotImplementedException();
+        }
+
+        void ISourceGenerator.Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
