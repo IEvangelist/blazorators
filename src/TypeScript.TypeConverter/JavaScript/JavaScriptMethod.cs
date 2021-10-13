7bc3d8522660cbb325265ed00af7ae4fa63d6577
David Pine <david.pine@microsoft.com>
2021-10-13 14:18:23 -0500

    Added triple slash comments on model types

--- src/TypeScript.TypeConverter/JavaScript/JavaScriptMethod.cs
+++ src/TypeScript.TypeConverter/JavaScript/JavaScriptMethod.cs
@@ -6,11 +6,23 @@ using TypeScript.TypeConverter.CSharp;
 namespace TypeScript.TypeConverter.JavaScript;
 
 internal record JavaScriptMethod(
-
-    string InvokableMethodName,
-
+    /// <summary>
+    /// The exact name of the JavaScript API method.
+    /// </summary>
     string JavaScriptApiMethodName,
 
+    /// <summary>
+    /// The invokable method name, when different than
+    /// the <paramref name="JavaScriptApiMethodName"/>
+    /// the method is not considered pure.
+    /// </summary>
+    string InvokableMethodName,
+
+    /// <summary>
+    /// The optional listing of method parameters.
+    /// </summary>
+    List<CSharpType>? Parameters = null)
+{
     /// <summary>
     /// A "pure" JavaScript method is one that
     /// can be invoked without custom JavaScript.
@@ -18,9 +30,9 @@ internal record JavaScriptMethod(
     /// <remarks>
     /// The following example is a "pure" JavaScript function.
     /// <code type="javascript">
-    /// window.prompt('')
+    /// window.prompt(message, default);
     /// </code>
+    /// This can be called from an <c>IJSRuntime</c> or <c>IJSObjectReference</c> instance directly.
     /// </remarks>
-    bool IsPure,
-
-    List<CSharpType> Parameters);
+    public bool IsPure => JavaScriptApiMethodName != InvokableMethodName;
+}
