3dfc62f1c49d9a1841cda11f1da9cf42bb5754bb
David Pine <david.pine@microsoft.com>
2021-10-13 14:00:38 -0500

    Added samples dir, and example of 'pure' JS method

--- src/TypeScript.TypeConverter/JavaScript/JavaScriptMethod.cs
+++ src/TypeScript.TypeConverter/JavaScript/JavaScriptMethod.cs
@@ -6,6 +6,21 @@ using TypeScript.TypeConverter.CSharp;
 namespace TypeScript.TypeConverter.JavaScript;
 
 internal record JavaScriptMethod(
+
     string InvokableMethodName,
+
     string JavaScriptApiMethodName,
+
+    /// <summary>
+    /// A "pure" JavaScript method is one that
+    /// can be invoked without custom JavaScript.
+    /// </summary>
+    /// <remarks>
+    /// The following example is a "pure" JavaScript function.
+    /// <code type="javascript">
+    /// window.prompt('')
+    /// </code>
+    /// </remarks>
+    bool IsPure,
+
     List<CSharpType> Parameters);
