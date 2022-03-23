1f275168e74b78af2d15fd72398bcf0e6bc41bb6
IEvangelist <david.pine@microsoft.com>
2022-03-23 18:32:31 +0000

    Deploying to gh-pages from  @ c09bcdfd002adf37e74daedc0a89c2186b8c7bcd 🚀

---
+++ _content/Blazor.Geolocation.WebAssembly/blazorators.geolocation.g.js
@@ -0,0 +1,59 @@
+// TODO: Consider source-generating this too.
+
+const onSuccess = (dotnetObj, successMethodName, position) => {
+    // HACK: Blazor isn't correctly deserializing these.
+    // It ignores the `JsonPropertyNameAttribute` :(
+    const result = {
+        Timestamp: position.timestamp,
+        Coords: {
+            Accuracy: position.coords.accuracy,
+            Altitude: position.coords.altitude,
+            AltitudeAccuracy: position.coords.altitudeAccuracy,
+            Heading: position.coords.heading,
+            Latitude: position.coords.latitude,
+            Longitude: position.coords.longitude,
+            Speed: position.coords.speed
+        }
+    };
+    dotnetObj.invokeMethod(successMethodName, result);
+    dotnetObj.dispose();
+};
+
+const onError = (dotnetObj, errorMethodName, error) => {
+    const result = {
+        Code: error.code,
+        Message: error.message,
+        PERMISSION_DENIED: error.PERMISSION_DENIED,
+        POSITION_UNAVAILABLE: error.POSITION_UNAVAILABLE,
+        TIMEOUT: error.TIMEOUT
+    };
+    dotnetObj.invokeMethod(errorMethodName, result);
+    dotnetObj.dispose();
+};
+
+const getCurrentPosition = (
+    dotnetObj,
+    successMethodName,
+    errorMethodName,
+    options) => {
+    navigator.geolocation.getCurrentPosition(
+        position => onSuccess(dotnetObj, successMethodName, position),
+        error => onError(dotnetObj, errorMethodName, error),
+        options);
+}
+
+const watchPosition = (
+    dotnetObj,
+    successMethodName,
+    errorMethodName,
+    options) => {
+    return navigator.geolocation.watchPosition(
+        position => onSuccess(dotnetObj, successMethodName, position),
+        error => onError(dotnetObj, errorMethodName, error),
+        options);
+}
+
+window.blazorators = {
+    getCurrentPosition,
+    watchPosition
+};
